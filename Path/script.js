93262e15c367587888a7d2752b582e6c5ea1957a
Lakshya Mudaliar <lkmcallmeb2195@gmail.com>
2024-10-29 17:16:08 +0530

    Path

---
+++ Path/script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Example of a simple function to show an alert
+function showAlert() {
+    alert("Welcome to the Data Science Learning Path!");
+}
+
+// Call the showAlert function on page load
+window.onload = showAlert;
